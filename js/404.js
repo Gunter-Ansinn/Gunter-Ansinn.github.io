e05727293e47a4e3786687baa76bc28397e9ea83
Günter Ansinn <100724671+Gunter-Ansinn@users.noreply.github.com>
2022-12-01 15:00:35 -0500

    earth

--- js/404.js
+++ js/404.js
@@ -1,24 +1,21 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new THREE.TextureLoader();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
+const renderer = createRenderer();
 
 const geometry = new THREE.SphereGeometry( 1, 32, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const black_hole = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshBasicMaterial( { map: loader.load('assets/earth.jpg'), } ); //color: 0x00ff00
+const earth = new THREE.Mesh( geometry, material );
 
-
-scene.add( black_hole );
+scene.add( earth );
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
 
-    black_hole.rotation.x += 0.01;
+    earth.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
---
+++ js/three-setup.js
@@ -0,0 +1,6 @@
+function createRenderer() {
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    return renderer;
+}
