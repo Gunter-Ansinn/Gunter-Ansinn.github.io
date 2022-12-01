a1754068db6300b496f9060c6d20a901019c2bd5
Günter Ansinn <100724671+Gunter-Ansinn@users.noreply.github.com>
2022-12-01 13:49:30 -0500

    fixed 404.js

--- js/404.js
+++ js/404.js
@@ -10,15 +10,6 @@ const geometry = new THREE.SphereGeometry( 1, 32, 16 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const black_hole = new THREE.Mesh( geometry, material );
 
-const black_hole_material = new THREE.ShaderMaterial({
-    uniforms : {
-        time: { value: 1.0},
-        resolution: { value: new THREE.Vector2() }
-    },
-
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('fragmentShader').textContent
-})
 
 scene.add( black_hole );
 
